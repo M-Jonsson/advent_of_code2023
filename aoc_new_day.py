d3bb5d7e83a137af2e07a130bcf96876da10a2e4
Mathias Jonsson <mathias.jonsson99@gmail.com>
2023-12-12 18:50:47 +0100

    New files now have some template code instead of being blank

--- aoc_new_day.py
+++ aoc_new_day.py
@@ -1,21 +1,32 @@
 import sys
+from os.path import exists
 
 
 def new_file(day_num: int):
-    """Generates files for a day of Advent of Code.
+    """Generates new files for a day of Advent of Code. 
     Generates a .py for the code and a .txt in /data to store the puzzle input
-    on the format day{day_num}.py and day{day_num}_input.txt.
+    on the format day{day_num}.py and day{day_num}_input.txt as long as the 
+    files do not already exist.
 
     Args:
         day_num (int): Day number to use in the file names.
     """    
-    # Python file
-    with open(f'day{day_num}.py', 'a'):
-        pass
-
-    # Input txt file
-    with open(f'data/day{day_num}_input.txt', 'a'):
-        pass
+    if not exists(f'day{day_num}.py'):
+        # Python file
+        lines = ['from aoc_common import read_file',
+                 'def solution1(data):', '\tpass',
+                 'def solution2(data):', '\tpass',
+                 'if __name__ == "__main__":',
+                 f'\tdata = read_file({day_num})',
+                 '\tprint(f"Solution to part 1: {solution1(data)}")',
+                 '\tprint(f"Solution to part 2: {solution2(data)}")']
+        with open(f'day{day_num}.py', 'w') as file:
+            file.write('\n'.join(str(line) for line in lines))
+        # Input txt file
+        with open(f'data/day{day_num}_input.txt', 'a'):
+            pass
+    else:
+        print('Python file already exists.')
 
 if __name__=='__main__':
     try:
